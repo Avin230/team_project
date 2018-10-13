81a1b69fde8b48c7c162e37f96e36ca85ef6f3a5
Avin230 <avin.keith@hgs.hiddengeniusproject.org>
2018-10-13 14:00:39 -0700

    successful

---
+++ Rich2.sh
@@ -0,0 +1,21 @@
+cd /Users/hgpmac77/Desktop
+mkdir "Rich2"
+cd Rich2
+mkdir "mindfulness" 
+cd mindfulness
+touch "mindfulness7-12-18.txt"
+touch "mindfulness7-11-18"
+touch "mindfulness7-10-18"
+touch "mindfulness7-09-18"
+cd ..
+mkdir "CSmindfulness"
+cd CSmindfulness
+cd ..
+mkdir "CSHW"
+cd CSHW
+touch "Chp 4 Terms" 
+touch "Chp 3 Terms" 
+touch "Chp 2 Terms" 
+touch "Chp 1 Terms"
+cd ..
+mkdir "CSProjects"
---
+++ week4-wed.sh
@@ -0,0 +1,17 @@
+cd Desktop
+mkdir week4-wed
+cd week4-wed
+touch index.html
+mkdir css img
+cd css
+touch style1.css
+touch style2.css
+cd ../
+cd img
+touch img1.jpg
+touch img2.jpg
+touch img3.jpg
+cd ../../
+curl -o img1.jpg "http://www.thathashtagshow.com/wp-content/uploads/2018/06/dragon_ball_super_next_gen_group_wallpaper_by_windyechoes-dbfvor0.png"
+curl -o img2.jpg "https://www.geek.com/wp-content/uploads/2018/03/Dragon-Ball-Super-Movie-625x352.jpg"
+curl -o img3.jpg "https://blogs-images.forbes.com/olliebarder/files/2017/10/goku_ultra_instinct.jpg"
